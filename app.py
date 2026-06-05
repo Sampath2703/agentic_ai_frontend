7014f187e2d9ed4e020d74215fd003e0f7625c20
Sampath2703 <naniganji2703@gmail.com>
2026-06-06 00:26:54 +0530

    agentci ai project

--- app.py
+++ app.py
@@ -73,12 +73,12 @@ if button:
     data = res.json()["msg"]
 
     if res.status_code == 200:
-        with st.form():
+        
 
 
-            st.warning(f"City: {data['city']}")
-            st.warning(f"Temperature: {data['temp']}°C")
-            st.warning(f"Humidity: {data['humidity']}%")
-            st.warning(f"Wind Speed: {data['wind_speed']} m/s")
+        st.warning(f"City: {data['city']}")
+        st.warning(f"Temperature: {data['temp']}°C")
+        st.warning(f"Humidity: {data['humidity']}%")
+        st.warning(f"Wind Speed: {data['wind_speed']} m/s")
 
-            st.warning(data["answer"])
+        st.warning(data["answer"])
