d5c7dd0b5e2863ecd05c0dd5e8506c2ac41a0dc5
Sampath2703 <naniganji2703@gmail.com>
2026-06-12 17:57:54 +0530

    errors

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 
-
 backend_url = st.secrets["backend_url"]
 
 st.markdown(
@@ -23,12 +22,11 @@ st.markdown(
         margin-bottom: 30px;
     }
 
-    /* Custom style for your side headings */
     .custom-label {
-        font-size: 38px; /* Adjust this number to make it as big as you want */
+        font-size: 38px;
         font-weight: 800;
         color: black;
-        margin-bottom: -35px; /* Pulls the heading closer to its text box */
+        margin-bottom: -35px;
     }
 
     .block-container {
@@ -47,33 +45,45 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Main Title
 st.markdown("<h1>🌦️ AI Weather Forecast</h1>", unsafe_allow_html=True)
 
-# City Section
 st.markdown('<p class="custom-label">City</p>', unsafe_allow_html=True)
 city = st.text_input("", key="city_input")
 
-# State Section
 st.markdown('<p class="custom-label">Question</p>', unsafe_allow_html=True)
 question = st.text_input("", key="question_input")
-
 
 button = st.button("Get Weather")
 
 if button:
     res = requests.post(
-    f"{backend_url}/get_weather",
-    json={
-        "city": city,
-        "question": question
-    }
+        f"{backend_url}/get_weather",
+        json={
+            "city": city,
+            "question": question
+        }
     )
 
-
     st.write("Status:", res.status_code)
-    st.write("Raw Response:")
-    st.code(res.text)
 
     if res.status_code == 200:
         data = res.json()["msg"]
+
+        st.success("Weather Report")
+
+        st.markdown(f"""
+### 📍 City: {data['city']}
+
+### 🌡️ Temperature: {data['temp']}°C  
+### 💧 Humidity: {data['humidity']}%  
+### 🌬️ Wind Speed: {data['wind_speed']} m/s  
+### ☁️ Condition: {data['weather']}
+
+---
+
+### 🤖 AI Advice:
+{data['answer']}
+""")
+    else:
+        st.error("Something went wrong")
+        st.code(res.text)
